d01c2718b4867f41778296f94c5d9f77173b83c1
xueshansheng <xueshansheng-it@bestpay.com.cn>
2020-05-25 18:46:29 +0800

    鉴权中心初始化

---
+++ authentication-center-web/src/main/java/com/ss/authentication/center/AuthenticationCenterApplication.java
@@ -0,0 +1,13 @@
+package com.ss.authentication.center;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationCenterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationCenterApplication.class, args);
+    }
+
+}
---
+++ authentication-center-web/src/test/java/com/ss/authentication/center/AuthenticationCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ss.authentication.center;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthenticationCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
